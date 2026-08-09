f0453fe78696dae785fa48d006fc1583a9812172
xofatechnology-droid <xofatechnology@gmail.com>
2026-08-09 09:09:00 +0000

    Remove route group, flatten admin routes

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,3 +1,5 @@
-export default function AdminRootLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
-}
+import AdminShell from '@/components/admin/AdminShell';
+
+export default function AdminLayout({ children }: { children: React.ReactNode }) {
+  return <AdminShell>{children}</AdminShell>;
+  }
